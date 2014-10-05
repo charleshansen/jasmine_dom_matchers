0b6d871e52305d2b64f23398bfdc0f5d5ff08b80
Charles Hansen <chansen87@gmail.com>
2014-10-05 11:06:12 -0700

    Add toHaveClass, support for selectors

--- spec/javascripts/MatchersSpec.js
+++ spec/javascripts/MatchersSpec.js
@@ -5,6 +5,28 @@ describe("Matchers", function() {
     return element.children;
   };
 
+  describe("matchers that expect a single element", function() {
+    it('works when given a collection with one element', function() {
+      expect($('<div class="foo"></div>')).toHaveClass('foo');
+    });
+
+    it('works when given a single html element', function() {
+      expect($('<div class="foo"></div>')[0]).toHaveClass('foo');
+    });
+
+    it('works when given a selector', function() {
+      document.querySelector('#jasmine-content').appendChild($('<div class="foo"></div>')[0]);
+      expect('.foo').toHaveClass('foo');
+    });
+  });
+
+  it('tests classes', function() {
+    expect($('<div class="foo bar"></div>')).toHaveClass('foo');
+    expect($('<div class="foo bar"></div>')).toHaveClass('bar');
+    expect($('<div class="foo"></div>')).not.toHaveClass('bar');
+    expect($('<div><div class="foo"></div></div>')).not.toHaveClass('foo');
+  });
+
   it('matches text', function() {
     expect($('<div>foo</div>')).toHaveText('foo');
     expect($('<div><span>foo</span><p></p></div>')).toHaveText('foo');
